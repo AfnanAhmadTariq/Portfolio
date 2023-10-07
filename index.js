865fe8ae92fedeeca5d9120b0a8293b6ab2bd4ef
programmerG <116060169+programerG@users.noreply.github.com>
2023-10-07 19:20:07 +0500

    Test v2

---
+++ index.js
@@ -0,0 +1,10 @@
+
+function showMenu() {
+    document.getElementsByClassName('open')[0].style.display = 'none';
+    document.getElementsByClassName('menu')[0].style.display = 'block';
+}
+
+function hideMenu() {
+    document.getElementsByClassName('open')[0].style.display = 'block';
+    document.getElementsByClassName('menu')[0].style.display = 'none';
+}
