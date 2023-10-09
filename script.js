6e13079761b24d817874a565c2d7ef7b43ba662f
AfnanAhmadTariq <116060169+AfnanAhmadTariq@users.noreply.github.com>
2023-10-09 14:06:15 +0500

    Update v1

---
+++ script.js
@@ -0,0 +1,40 @@
+const backgroundElement = document.querySelector('.background');
+const typingTextElement = document.getElementById('typing-text');
+
+const images = ['images/Top-Software-Developer-Skills-to-Learn-This-Year-for-Tech-Jobs-scaled.jpg', 'images/1696229244943.gif'];
+let currentImageIndex = 0;
+
+const texts = ['I am a Dev.', 'I am a Gamer.', 'I am a Artist.' ,'I am a Game dev.', 'I am a Ethical Hacker.'];
+let currentTextIndex = 0;
+
+function changeBackgroundImage() {
+  currentImageIndex = (currentImageIndex + 1) % images.length;
+  const imageUrl = images[currentImageIndex];
+  backgroundElement.style.backgroundImage = `url(${imageUrl})`;
+}
+
+function typeEffect() {
+  const currentText = texts[currentTextIndex];
+  typingTextElement.textContent = '';
+
+  let index = 0;
+  const typingInterval = setInterval(() => {
+    typingTextElement.textContent += currentText[index];
+    index++;
+
+    if (index >= currentText.length) {
+      clearInterval(typingInterval);
+      setTimeout(() => {
+        currentTextIndex = (currentTextIndex + 1) % texts.length;
+        typeEffect();
+      }, 2000); // Delay before typing the next text
+    }
+  }, 100); // Typing speed (milliseconds per character)
+}
+
+// Initial setup
+changeBackgroundImage();
+typeEffect();
+
+// Change background image every 5 seconds
+setInterval(changeBackgroundImage, 5000);
